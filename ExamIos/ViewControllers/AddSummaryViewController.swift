34a0936ed0e05a7a551c3188e0318fc49c22bf71
Lukas Kindt <lukas.kindt@student.hogent.be>
2021-08-13 14:05:55 +0200

    Add Summary Finished

--- ExamIos/ViewControllers/AddSummaryViewController.swift
+++ ExamIos/ViewControllers/AddSummaryViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class AddSummaryViewController: UIViewController {
     
+    let data = Data.shared
+    
+    @IBOutlet weak var txtTitle: UITextField!
+    @IBOutlet weak var txtSummary: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func buttonWasPressed(_ sender: Any) {
+        if(txtTitle.hasText && txtSummary.hasText){
+            let summary = Summary(name: txtTitle.text!, summary: txtSummary.text)
+            data.summaries.append(summary)
+
+            dismiss(animated: true, completion: nil)
+        }
+    }
 }
